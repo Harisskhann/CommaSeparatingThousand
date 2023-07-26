2476b865b57f9b6d82e04da4fb3d9c47d95a9359
M.Haris Khan <hkhan@cedrus.com>
2023-07-26 16:15:43 +0500

    Feat;CommaSeparatingThousand

---
+++ CommaSeparatingThousand/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+public class Program
+{
+    public static string FormatNum(int number)
+    {
+        string numString = number.ToString();
+        int length = numString.Length;
+        string formattedNumber = "";
+
+        for (int i = 0; i < length; i++)
+        {
+            formattedNumber += numString[i];
+
+            int remainingDigits = length - i - 1;
+            if (remainingDigits > 0 && remainingDigits % 3 == 0)
+            {
+                formattedNumber += ",";
+            }
+        }
+
+        return formattedNumber;
+
+    }
+    public static void Main()
+    {
+        int num;
+        Console.WriteLine("Enter a number");
+        num = Convert.ToInt32(Console.ReadLine());
+        string value= FormatNum(num);
+        Console.WriteLine(value);
+        Console.ReadLine();
+    }
+}
